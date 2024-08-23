3c49f93b500ae8de8e9290958b86b80c128f9d07
lucien <2172856294@qq.com>
2024-08-22 19:45:09 -0700

    add experiment scripts to compare to Prover

---
+++ experiment/gen_benchs.sh
@@ -0,0 +1,10 @@
+perl  vraps_to_silver.pl gadgets/ISW/mult/gadget_mult_2_shares.sage > experiment/nl/ISW_mult/2shares.nl
+perl  vraps_to_silver.pl gadgets/ISW/mult/gadget_mult_3_shares.sage > experiment/nl/ISW_mult/3shares.nl
+perl  vraps_to_silver.pl gadgets/ISW/mult/gadget_mult_4_shares.sage > experiment/nl/ISW_mult/4shares.nl
+perl  vraps_to_silver.pl gadgets/ISW/mult/gadget_mult_5_shares.sage > experiment/nl/ISW_mult/5shares.nl
+perl  vraps_to_silver.pl gadgets/ISW/mult/gadget_mult_6_shares.sage > experiment/nl/ISW_mult/6shares.nl
+
+perl  vraps_to_silver.pl gadgets/nlogn/gadget_refresh_2_shares.sage > experiment/nl/refresh_nlogn/2shares.nl
+perl  vraps_to_silver.pl gadgets/nlogn/gadget_refresh_4_shares.sage > experiment/nl/refresh_nlogn/4shares.nl
+perl  vraps_to_silver.pl gadgets/nlogn/gadget_refresh_6_shares.sage > experiment/nl/refresh_nlogn/6shares.nl
+perl  vraps_to_silver.pl gadgets/nlogn/gadget_refresh_7_shares.sage > experiment/nl/refresh_nlogn/7shares.nl
---
+++ experiment/run_im.sh
@@ -0,0 +1,23 @@
+./ironmask gadgets/ISW/mult/gadget_mult_2_shares.sage NI -t 1 -j 4 >> experiment/im_results/ISW_mult/ng_2_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_3_shares.sage NI -t 2 -j 4 >> experiment/im_results/ISW_mult/ng_3_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_4_shares.sage NI -t 3 -j 4 >> experiment/im_results/ISW_mult/ng_4_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_5_shares.sage NI -t 4 -j 4 >> experiment/im_results/ISW_mult/ng_5_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_6_shares.sage NI -t 5 -j 4 >> experiment/im_results/ISW_mult/ng_6_shares.txt
+
+./ironmask gadgets/ISW/mult/gadget_mult_2_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/ISW_mult/g_2_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_3_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/ISW_mult/g_3_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_4_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/ISW_mult/g_4_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_5_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/ISW_mult/g_5_shares.txt
+./ironmask gadgets/ISW/mult/gadget_mult_6_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/ISW_mult/g_6_shares.txt
+
+./ironmask gadgets/nlogn/gadget_refresh_2_shares.sage NI -t 1 -j 4 >> experiment/im_results/nlogn/ng_2_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_4_shares.sage NI -t 3 -j 4 >> experiment/im_results/nlogn/ng_4_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_6_shares.sage NI -t 5 -j 4 >> experiment/im_results/nlogn/ng_6_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_7_shares.sage NI -t 6 -j 4 >> experiment/im_results/nlogn/ng_7_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_8_shares.sage NI -t 7 -j 4 >> experiment/im_results/nlogn/ng_8_shares.txt
+
+./ironmask gadgets/nlogn/gadget_refresh_2_shares.sage NI -t 1 -j 4 --glitch >> experiment/im_results/nlogn/g_2_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_4_shares.sage NI -t 3 -j 4 --glitch >> experiment/im_results/nlogn/g_4_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_6_shares.sage NI -t 5 -j 4 --glitch >> experiment/im_results/nlogn/g_6_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_7_shares.sage NI -t 6 -j 4 --glitch >> experiment/im_results/nlogn/g_7_shares.txt
+./ironmask gadgets/nlogn/gadget_refresh_8_shares.sage NI -t 7 -j 4 --glitch >> experiment/im_results/nlogn/g_8_shares.txt
